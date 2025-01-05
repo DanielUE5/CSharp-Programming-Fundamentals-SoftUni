c79c6645a19a474940d3770e604e412ecb3281d2
Daniel <dani94@mail.bg>
2025-01-05 02:58:21 +0200

    Add files via upload

---
+++ Divisible by 3/Divisible by 3/Program.cs
@@ -0,0 +1,10 @@
+﻿class Divisible_by_3
+{
+    static void Main()
+    {
+        for (int i = 3; i <= 100; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Theatre Promotion/Theatre Promotion/Program.cs
@@ -0,0 +1,58 @@
+﻿class Theatre_Promotions
+{
+    static void Main()
+    {
+        string day = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        if (age > 122 || age < 0)
+        {
+            Console.WriteLine("Error!");
+        }
+
+        if (day == "Weekday") {
+            if (age >= 0 && age <= 18)
+            {
+                Console.WriteLine("12$");
+            }
+            else if (age > 18 && age <= 64)
+            {
+                Console.WriteLine("18$");
+            }
+            else if (age > 64 && age <= 122)
+            {
+                Console.WriteLine("12$");
+            }
+        }
+        if (day == "Weekend")
+        {
+            if (age >= 0 && age <= 18)
+            {
+                Console.WriteLine("15$");
+            }
+            else if (age > 18 && age <= 64)
+            {
+                Console.WriteLine("20$");
+            }
+            else if (age > 64 && age <= 122)
+            {
+                Console.WriteLine("15$");
+            }
+        }
+        if (day == "Holiday")
+        {
+            if (age >= 0 && age <= 18)
+            {
+                Console.WriteLine("5$");
+            }
+            else if (age > 18 && age <= 64)
+            {
+                Console.WriteLine("12$");
+            }
+            else if (age > 64 && age <= 122)
+            {
+                Console.WriteLine("10$");
+            }
+        }
+    }
+}
