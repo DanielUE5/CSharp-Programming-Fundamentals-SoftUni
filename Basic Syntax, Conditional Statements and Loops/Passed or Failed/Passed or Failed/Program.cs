0bbdf68c8579aeb1abaa48d616afc1fc0e87c043
Daniel <dani94@mail.bg>
2025-01-18 03:04:41 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Passed or Failed/Passed or Failed/Program.cs
@@ -0,0 +1,16 @@
+﻿class Passed_or_Failed
+{
+    static void Main()
+    {
+        float number = float.Parse(Console.ReadLine());
+
+        if (number >= 3)
+        {
+            Console.WriteLine("Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Failed!");
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops/Passed/Passed/Program.cs
@@ -0,0 +1,12 @@
+﻿class Passed
+{
+    static void Main()
+    {
+        float number = float.Parse(Console.ReadLine());
+
+        if (number >= 3)
+        {
+            Console.WriteLine("Passed!");
+        } 
+    }
+}
