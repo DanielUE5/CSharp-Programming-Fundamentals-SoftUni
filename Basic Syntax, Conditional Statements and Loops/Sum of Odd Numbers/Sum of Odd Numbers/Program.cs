362460c4c010cddc3c578174b7f09016befdae18
Daniel <dani94@mail.bg>
2025-01-18 03:06:47 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Sum of Odd Numbers/Sum of Odd Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿class Sum_of_Odd_Numbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 1; count < n; i++)
+        {
+            if(i % 2 == 1)
+            {
+                Console.WriteLine(i);
+                count++;
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum: " + sum);
+    }
+}
