344b7f92f6008b46c667ee1217b4ae4853927dc2
Daniel <dani94@mail.bg>
2025-02-25 23:23:13 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercises/Rage Expenses/Rage Expenses/Program.cs
@@ -0,0 +1,49 @@
+﻿class Rage_Expenses
+{
+    static void Main()
+    {
+        int lostGamesCount = int.Parse(Console.ReadLine());
+        float headsetPrice = float.Parse(Console.ReadLine());
+        float mousePrice = float.Parse(Console.ReadLine());
+        float keyboardPrice = float.Parse(Console.ReadLine());
+        float displayPrice = float.Parse(Console.ReadLine());
+
+        int headsetsTrashed = 0;
+        int miceTrashed = 0;
+        int keyboardsTrashed = 0;
+        int displaysTrashed = 0;
+
+        for (int game = 1; game <= lostGamesCount; game++)
+        {
+            bool headsetTrashed = false;
+            bool mouseTrashed = false;
+
+            if (game % 2 == 0)
+            {
+                headsetsTrashed++;
+                headsetTrashed = true;
+            }
+
+            if (game % 3 == 0)
+            {
+                miceTrashed++;
+                mouseTrashed = true;
+            }
+
+            if (headsetTrashed && mouseTrashed)
+            {
+                keyboardsTrashed++;
+
+                if (keyboardsTrashed % 2 == 0)
+                {
+                    displaysTrashed++;
+                }
+            }
+        }
+        double totalExpenses = (headsetsTrashed * headsetPrice) +
+                               (miceTrashed * mousePrice) +
+                               (keyboardsTrashed * keyboardPrice) +
+                               (displaysTrashed * displayPrice);
+        Console.WriteLine($"Rage expenses: {totalExpenses:F2} lv.");
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Exercises/Triangle of Numbers/Triangle of Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿class Triangle_of_Numbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
