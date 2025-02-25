b758c49692b4e17827184c605889c1aff4ae9b81
Daniel <dani94@mail.bg>
2025-02-25 23:21:16 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercises/Print and sum/Print and sum/Program.cs
@@ -0,0 +1,16 @@
+﻿class Print_and_Sum
+{
+    static void Main()
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        for (int i = num1; i <= num2; i++) {
+            Console.Write(i + " ");
+            count += i;
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Sum: {count}");
+    } 
+}
