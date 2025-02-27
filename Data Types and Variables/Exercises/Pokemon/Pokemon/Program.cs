83d66f712bb07ef62e3e2b013b89281351e28a39
Daniel <dani94@mail.bg>
2025-02-27 12:43:06 +0200

    Add files via upload

---
+++ Data Types and Variables/Exercises/Pokemon/Pokemon/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int M = int.Parse(Console.ReadLine());
+        int Y = int.Parse(Console.ReadLine());
+
+        int targetCount = 0;
+        int originalN = N;
+
+        while (N >= M)
+        {
+            N -= M;
+            targetCount++;
+
+            if (N == originalN / 2 && Y != 0)
+            {
+                N /= Y;
+            }
+        }
+        Console.WriteLine(N);
+        Console.WriteLine(targetCount);
+    }
+}
---
+++ Data Types and Variables/Exercises/Snowballs/Snowballs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+class Snowballs
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        BigInteger maxSnowballValue = 0;
+        int bestSnowballSnow = 0;
+        int bestSnowballTime = 0;
+        int bestSnowballQuality = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            int snowballSnow = int.Parse(Console.ReadLine());
+            int snowballTime = int.Parse(Console.ReadLine());
+            int snowballQuality = int.Parse(Console.ReadLine());
+
+            BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+
+            if (snowballValue > maxSnowballValue)
+            {
+                maxSnowballValue = snowballValue;
+                bestSnowballSnow = snowballSnow;
+                bestSnowballTime = snowballTime;
+                bestSnowballQuality = snowballQuality;
+            }
+        }
+
+        Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {maxSnowballValue} ({bestSnowballQuality})");
+    }
+}
