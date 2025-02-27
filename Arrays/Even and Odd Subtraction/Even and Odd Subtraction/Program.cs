3df71d1f78f269ceb674734aa6b1e03d7673d7cd
Daniel <dani94@mail.bg>
2025-02-27 21:43:42 +0200

    Add files via upload

---
+++ Arrays/Even and Odd Subtraction/Even and Odd Subtraction/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace EvenOddSubtraction
+{
+    public class EvenOddSubtraction
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+                else
+                {
+                    oddSum += numbers[i];
+                }
+            }
+            int difference = evenSum - oddSum;
+            Console.WriteLine(evenSum - oddSum);
+        }
+    }
+}
