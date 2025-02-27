a27bea4d461db9629ba758385575667b3e490645
Daniel <dani94@mail.bg>
2025-02-27 12:29:17 +0200

    Add files via upload

---
+++ Data Types and Variables - Lab/Chars to String/Chars to String/Program.cs
@@ -0,0 +1,13 @@
+﻿class CharsToString
+{
+    static void Main()
+    {
+        char input1 = char.Parse(Console.ReadLine());
+        char input2 = char.Parse(Console.ReadLine());
+        char input3 = char.Parse(Console.ReadLine());
+
+        string result = input1.ToString() + input2.ToString() + input3.ToString();
+
+        Console.WriteLine(result);
+    }
+}
