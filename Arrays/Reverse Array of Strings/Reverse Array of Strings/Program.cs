0dd44f99667fe31967a27aaa4d013eff370f9701
Daniel <dani94@mail.bg>
2025-02-27 20:50:35 +0200

    Add files via upload

---
+++ Arrays/Reverse Array of Strings/Reverse Array of Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿class ReverseArrayOfStrings
+{
+    static void Main()
+    {
+        var items = Console.ReadLine()
+        .Split(' ')
+        .ToArray();
+
+        for (int i = 0; i < items.Length / 2; i++)
+        {
+            var oldElement = items[i];
+            items[i] = items[items.Length - 1 - i];
+            items[items.Length - 1 - i] = oldElement;
+        }
+
+        Console.WriteLine(string.Join(" ", items));
+    }
+}
---
+++ Arrays/Rounding Numbers/Rounding Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿class RoundedNumbers
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        double[] numbers = input.Split(' ').Select(double.Parse).ToArray();
+
+        foreach (double num in numbers)
+        {
+            int rounded = (int)Math.Round(num, MidpointRounding.AwayFromZero);
+            Console.WriteLine($"{num} => {rounded}");
+        }
+    }
+}
