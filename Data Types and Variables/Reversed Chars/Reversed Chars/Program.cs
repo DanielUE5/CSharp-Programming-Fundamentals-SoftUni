e483057fef22782d067f413e065f54fa3f33103e
Daniel <dani94@mail.bg>
2025-02-27 12:37:00 +0200

    Add files via upload

---
+++ Data Types and Variables/Reversed Chars/Reversed Chars/Program.cs
@@ -0,0 +1,12 @@
+﻿class Reverse_Chars
+{
+    static void Main()
+    {
+
+        string line1 = Console.ReadLine();
+        string line2 = Console.ReadLine();
+        string line3 = Console.ReadLine();
+
+        Console.WriteLine($"{line3} {line2} {line1}");
+    }
+}
