d3aef14c95de404fa6c972574308849969d106ea
Daniel <dani94@mail.bg>
2025-02-27 20:51:48 +0200

    Add files via upload

---
+++ Arrays/Sum Even Numbers/Sum Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿class SumEvenNumbers 
+{
+    static void Main()
+    {
+        var numbers = Console.ReadLine();
+        int sum = 0;
+
+        int[] arr = numbers.Split(' ').Select(int.Parse).ToArray();
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            int currentNumber = arr[i];
+
+            if (currentNumber % 2 == 0)
+            {
+                sum += currentNumber;
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
