9e7d0242ce5753b2b53b6faad3661eea2ffa5085
Daniel <dani94@mail.bg>
2025-03-02 22:24:18 +0200

    Add files via upload

---
+++ Equal Sums/Equal Sums/Program.cs
@@ -0,0 +1,26 @@
+﻿class EqualSums
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int n = array.Length;
+
+        int totalSum = array.Sum();
+        int leftSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int rightSum = totalSum - leftSum - array[i];
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine(i);
+                return;
+            }
+
+            leftSum += array[i];
+        }
+
+        Console.WriteLine("no");
+    }
+}
---
+++ Top Integers/Top Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿class TopIntegers
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int n = array.Length;
+        int[] topIntegers = new int[n];
+        int topCount = 0;
+
+        int maxFromRight = int.MinValue;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (array[i] > maxFromRight)
+            {
+                topIntegers[topCount] = array[i];
+                topCount++;
+                maxFromRight = array[i];
+            }
+        }
+
+        for (int i = topCount - 1; i >= 0; i--)
+        {
+            Console.Write(topIntegers[i]);
+            if (i > 0)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
