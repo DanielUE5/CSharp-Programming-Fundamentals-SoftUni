71c9b07076d00fbbbc9577cd78b99c4ae0622aea
Daniel <dani94@mail.bg>
2025-03-02 22:25:16 +0200

    Add files via upload

---
+++ Magic Sum/Magic Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿class MagicSum
+{
+    static void Main()
+    {
+        int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int target = int.Parse(Console.ReadLine().Trim());
+        FindAndPrintPairs(array, target);
+    }
+
+    static void FindAndPrintPairs(int[] array, int target)
+    {
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] + array[j] == target)
+                {
+                    Console.WriteLine(array[i] + " " + array[j]);
+                }
+            }
+        }
+    }
+}
