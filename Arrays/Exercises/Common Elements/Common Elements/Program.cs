2ba80d1d79b13077e9ee91185ab3010cbbbe22ec
Daniel <dani94@mail.bg>
2025-03-02 22:23:24 +0200

    Add files via upload

---
+++ Arrays/Exercises/Common Elements/Common Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿class CommonElements
+{
+    static void Main()
+    {
+        string[] firstArray = Console.ReadLine().Split(' ');
+        string[] secondArray = Console.ReadLine().Split(' ');
+
+        var commonElements = secondArray.Intersect(firstArray);
+
+        Console.WriteLine(string.Join(" ", commonElements));
+    }
+}
---
+++ Arrays/Exercises/Train/Train/Program.cs
@@ -0,0 +1,24 @@
+﻿class Train
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] wagons = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            wagons[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(string.Join(" ", wagons));
+
+        int totalPassengers = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            totalPassengers += wagons[i];
+        }
+
+        Console.WriteLine(totalPassengers);
+    }
+}
