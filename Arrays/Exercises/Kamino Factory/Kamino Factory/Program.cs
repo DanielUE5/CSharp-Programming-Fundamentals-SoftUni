c05502caeb4df6eff1b056cb634a290f507307a5
Daniel <dani94@mail.bg>
2025-04-22 19:07:22 +0300

    Add files via upload

---
+++ Arrays/Exercises/Kamino Factory/Kamino Factory/Program.cs
@@ -0,0 +1,64 @@
+﻿class KaminoFactory
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        string input;
+        int bestLength = -1;
+        int bestStartIndex = length;
+        int bestSum = -1;
+        int[] bestSequence = null;
+        int bestSequenceIndex = -1;
+        int currentSequenceIndex = 0;
+
+        while ((input = Console.ReadLine()) != "Clone them!")
+        {
+            currentSequenceIndex++;
+            int[] dnaSequence = input.Split(new char[] { '!', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+            int maxOnesLength = 0;
+            int currentOnesLength = 0;
+            int currentStartIndex = 0;
+            int currentSum = dnaSequence.Sum();
+
+            for (int i = 0; i < dnaSequence.Length; i++)
+            {
+                if (dnaSequence[i] == 1)
+                {
+                    currentOnesLength++;
+                }
+                else
+                {
+                    if (currentOnesLength > maxOnesLength)
+                    {
+                        maxOnesLength = currentOnesLength;
+                        currentStartIndex = i - currentOnesLength;
+                    }
+                    currentOnesLength = 0;
+                }
+            }
+
+            if (currentOnesLength > maxOnesLength)
+            {
+                maxOnesLength = currentOnesLength;
+                currentStartIndex = dnaSequence.Length - currentOnesLength;
+            }
+
+            if (maxOnesLength > bestLength ||
+                (maxOnesLength == bestLength && currentStartIndex < bestStartIndex) ||
+                (maxOnesLength == bestLength && currentStartIndex == bestStartIndex && currentSum > bestSum))
+            {
+                bestLength = maxOnesLength;
+                bestStartIndex = currentStartIndex;
+                bestSum = currentSum;
+                bestSequence = dnaSequence;
+                bestSequenceIndex = currentSequenceIndex;
+            }
+        }
+
+        Console.WriteLine($"Best DNA sample {bestSequenceIndex} with sum: {bestSum}.");
+        Console.WriteLine(string.Join(" ", bestSequence));
+    }
+}
---
+++ Arrays/Exercises/MaxSeqOfEqualElements/MaxSeqOfEqualElements/Program.cs
@@ -0,0 +1,47 @@
+﻿class MaxSeqOfEqualElemtns
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int n = array.Length;
+
+        int maxLength = 0;
+        int maxLengthElement = array[0];
+        int currentLength = 1;
+        int currentElement = array[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            if (array[i] == currentElement)
+            {
+                currentLength++;
+            }
+            else
+            {
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                    maxLengthElement = currentElement;
+                }
+                currentElement = array[i];
+                currentLength = 1;
+            }
+        }
+
+        if (currentLength > maxLength)
+        {
+            maxLength = currentLength;
+            maxLengthElement = currentElement;
+        }
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            Console.Write(maxLengthElement);
+            if (i < maxLength - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
