aba1517fb0111d48b12a9db7925f47cb5ae317ef
Daniel <dani94@mail.bg>
2025-04-22 18:54:01 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Exercises/Padawan Equipment/Padawan Equipment/Program.cs
@@ -0,0 +1,32 @@
+﻿class PadawanEquipment
+{
+    static void Main()
+    {
+        double amountOfMoney = double.Parse(Console.ReadLine());
+        int numStudents = int.Parse(Console.ReadLine());
+        double priceLightsaber = double.Parse(Console.ReadLine());
+        double priceRobe = double.Parse(Console.ReadLine());
+        double priceBelt = double.Parse(Console.ReadLine());
+
+        int lightsabersNeeded = (int)Math.Ceiling(numStudents * 1.10);
+        double totalCostLightsabers = lightsabersNeeded * priceLightsaber;
+
+        double totalCostRobes = numStudents * priceRobe;
+
+        int freeBelts = numStudents / 6;
+        int beltsNeeded = numStudents - freeBelts;
+        double totalCostBelts = beltsNeeded * priceBelt;
+
+        double totalCost = totalCostLightsabers + totalCostRobes + totalCostBelts;
+
+        if (totalCost <= amountOfMoney)
+        {
+            Console.WriteLine($"The money is enough - it would cost {totalCost:F2}lv.");
+        }
+        else
+        {
+            double neededMoney = totalCost - amountOfMoney;
+            Console.WriteLine($"John will need {neededMoney:F2}lv more.");
+        }
+    }
+}
