1416778fe4eeb0ebc2addad0f73ccb82343d228c
Daniel <dani94@mail.bg>
2025-08-21 03:39:28 +0300

    Add files via upload

---
+++ Methods/Math Operations/Math Operations/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Math_Operations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+
+            string op = Console.ReadLine();
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            double result = Calculate(num1, num2, op);
+            Console.WriteLine(result);
+        }
+            public static double Calculate(int number1, int number2, string operation)
+            {
+                double result = 0;
+
+                result = operation switch
+                {
+                    "+" => number1 + number2,
+                    "-" => number1 - number2,
+                    "*" => number1 * number2,
+                    "/" => number1 / (double)number2,
+                    "%" => number1 % number2,
+                    "^" => Math.Pow(number1, number2),
+                    _ => throw new InvalidOperationException("Invalid operation")
+                };
+                return result;
+        }
+
+    }
+}
+
---
+++ Methods/Multiply Evens by Odds/Multiply Evens by Odds/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Multiply_Evens_by_Odds
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                number = Math.Abs(number); // Convert negative to positive
+            }
+            int result = GetMultiplyEvensByOdds(number);
+            Console.WriteLine(result);
+        }
+
+        private static int GetMultiplyEvensByOdds(int number)
+        {
+        int evenSum = GetSumOfEvenDigits(number);
+        int oddSum = GetSumOfOddDigits(number);
+        return evenSum * oddSum;
+        }
+
+        private static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == 0)
+                {
+                    sum += digit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+
+        private static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 != 0)
+                {
+                    sum += digit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
+
