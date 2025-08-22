782222271a2294aab308b99e9ea251897f34305c
Daniel <dani94@mail.bg>
2025-08-22 05:08:33 +0300

    Add files via upload

---
+++ Lists/Sum Adjacent Equal Numbers/Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Sum_Adjacent_Equal_Numbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] += numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+                    i = -1; // Reset index to start checking from the beginning again
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
