31447fae18cc8b0b9eb374b76af97385b0d8c856
Daniel <dani94@mail.bg>
2025-08-25 19:46:39 +0300

    Add files via upload

---
+++ Lists/List of Products/List of Products/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace List_of_Products
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine()!);
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < quantity; i++)
+            {
+                string product = Console.ReadLine()!;
+                products.Add(product);
+            }
+
+            products.Sort(); // Sorting the list in alphabetical order
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
---
+++ Lists/Merging Lists/Mergin Lists/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Merging_Lists
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> row1 = Console.ReadLine()!
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> row2 = Console.ReadLine()!
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> merged = new List<int>();
+
+            int minLength = Math.Min(row1.Count, row2.Count); // Find the length of the shorter list
+
+            for (int i = 0; i < minLength; i++) // Iterate up to the length of the shorter list
+            {
+                merged.Add(row1[i]);
+                merged.Add(row2[i]);
+            }
+
+            if (row1.Count > minLength) // If row1 is longer, add the remaining elements
+            {
+                merged.AddRange(row1.GetRange(minLength, row1.Count - minLength));
+            }
+            else if (row2.Count > minLength) // If row2 is longer, add the remaining elements
+            {
+                merged.AddRange(row2.GetRange(minLength, row2.Count - minLength));
+            }
+            Console.WriteLine(string.Join(" ", merged));
+        }
+    }
+}
