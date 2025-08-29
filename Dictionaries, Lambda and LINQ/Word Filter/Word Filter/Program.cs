35efa1456c1b52fb75ef12c3bcc1ca861049268a
Daniel <dani94@mail.bg>
2025-08-29 19:20:02 +0300

    Add files via upload

---
+++ Dictionaries, Lambda and LINQ/Word Filter/Word Filter/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Word_Filter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] wordsToFilter = Console.ReadLine()
+                .Split()
+                .Where(w => w.Length % 2 == 0) // Filter words with even length
+                .ToArray(); // Convert the result to an array
+
+            Console.WriteLine(string.Join(Environment.NewLine, wordsToFilter)); // Print each word on a new line
+        }
+    }
+}
