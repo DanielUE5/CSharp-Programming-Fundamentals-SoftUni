b4d5d661c76c0736fac1d764b31dc76ca44f43af
Daniel <dani94@mail.bg>
2025-08-29 19:25:42 +0300

    Add files via upload

---
+++ Dictionaries, Lambda and LINQ/Exercises/SoftUni Parking/SoftUni Parking/Program.cs
@@ -0,0 +1,49 @@
+﻿class SoftUniParking
+{
+    static void Main()
+    {
+        Dictionary<string, string> parkingLot = new Dictionary<string, string>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] commandParts = Console.ReadLine().Split();
+
+            string action = commandParts[0];
+            string username = commandParts[1];
+
+            if (action == "register")
+            {
+                string licensePlateNumber = commandParts[2];
+
+                if (parkingLot.ContainsKey(username))
+                {
+                    Console.WriteLine($"ERROR: already registered with plate number {parkingLot[username]}");
+                }
+                else
+                {
+                    parkingLot[username] = licensePlateNumber;
+                    Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                }
+            }
+            else if (action == "unregister")
+            {
+                if (!parkingLot.ContainsKey(username))
+                {
+                    Console.WriteLine($"ERROR: user {username} not found");
+                }
+                else
+                {
+                    parkingLot.Remove(username);
+                    Console.WriteLine($"{username} unregistered successfully");
+                }
+            }
+        }
+
+        foreach (var user in parkingLot)
+        {
+            Console.WriteLine($"{user.Key} => {user.Value}");
+        }
+    }
+}
---
+++ Dictionaries, Lambda and LINQ/Exercises/Student Academy/Student Academy/Program.cs
@@ -0,0 +1,36 @@
+﻿class StudentAcademy
+{
+    static void Main()
+    {
+        Dictionary<string, List<double>> studentGrades = new Dictionary<string, List<double>>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string name = Console.ReadLine();
+            double grade = double.Parse(Console.ReadLine());
+
+            if (!studentGrades.ContainsKey(name))
+            {
+                studentGrades[name] = new List<double>();
+            }
+
+            studentGrades[name].Add(grade);
+        }
+
+        var filteredStudents = studentGrades
+            .Select(s => new
+            {
+                Name = s.Key,
+                AverageGrade = s.Value.Average()
+            })
+            .Where(s => s.AverageGrade >= 4.50)
+            .ToList();
+
+        foreach (var student in filteredStudents)
+        {
+            Console.WriteLine($"{student.Name} -> {student.AverageGrade:F2}");
+        }
+    }
+}
