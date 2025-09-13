1826653a3e49654cae920c27ea2a1111d9dcaf58
Daniel <dani94@mail.bg>
2025-09-13 23:33:35 +0300

    Add files via upload

---
+++ Text Processing/Digits, Letters and Others/Digits, Letters and Others/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Digits__Letters_and_Others
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            List<char> digits = new List<char>(); // List to store digit characters
+            List<char> letters = new List<char>(); // List to store letter characters
+            List<char> others = new List<char>(); // List to store other characters
+
+            foreach (char c in str) // Iterate through each character in the input string
+            {
+                if (char.IsDigit(c))
+                {
+                    digits.Add(c);
+                }
+                else if (char.IsLetter(c))
+                {
+                    letters.Add(c);
+                }
+                else
+                {
+                    others.Add(c);
+                }
+            }
+
+            foreach (char d in digits)
+            {
+                Console.Write(d); // Print digits without newline
+            }
+
+            Console.WriteLine();
+            foreach (char l in letters)
+            {
+                Console.Write(l); // Print letters without newline
+            }
+
+            Console.WriteLine();
+            foreach (char o in others)
+            {
+                Console.Write(o); // Print others without newline
+            }
+        }
+    }
+}
---
+++ Text Processing/Text Filter/Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Text_Filter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var banned in bannedWords)
+            {
+                text = text.Replace(banned, new string('*', banned.Length));
+            }
+            Console.WriteLine();
+            Console.WriteLine(text); 
+        }
+    }
+}
